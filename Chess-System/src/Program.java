abf2a37c17539a2d8e652183226af76613f3528a
Jefferson Sombra <soombra73@gmail.com>
2022-11-05 13:38:56 -0300

    First class created: Position

---
+++ Chess-System/src/Program.java
@@ -0,0 +1,8 @@
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(10, 3);
+        System.out.println(pos);
+    }
+}
